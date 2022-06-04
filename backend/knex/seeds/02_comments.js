04b1614c20be13cdabb25085bc93fb468a546128
rkrux <5960750+rkrux@users.noreply.github.com>
2022-06-05 01:35:38 +0530

    added comment datetime while seeding

--- backend/knex/seeds/02_comments.js
+++ backend/knex/seeds/02_comments.js
@@ -4,21 +4,22 @@
  */
 export async function seed(knex) {
   // Deletes ALL existing entries
-  await knex("comments").del();
-  await knex("comments").insert([
+  await knex('comments').del();
+  await knex('comments').insert([
     {
       commentText:
-        "Nine of us now seem to feel that the defendant is innocent, but we're just gambling on probabilities. \
-        We may be wrong. We may be trying to return a guilty man to the community. No one can really know. \
-        But we have a reasonable doubt, and this is a safeguard that has enormous value in our system. \
-        No jury can declare a man guilty unless it's sure. We nine can't understand how you three are still so sure. \
-        Maybe you can tell us.",
+        "Nine of us now seem to feel that the defendant is innocent, but we're just gambling on probabilities. We may \
+         be wrong. We may be trying to return a guilty man to the community. No one can really know. But we have a \
+         reasonable doubt, and this is a safeguard that has enormous value in our system. No jury can declare a man guilty \
+         unless it's sure. We nine can't understand how you three are still so sure. Maybe you can tell us.",
       userId: 8,
+      createdAt: '2022-06-04T19:51:24.693Z',
     },
     {
       commentText:
-        "It takes a great deal of courage to stand alone even if you believe in something very strongly.",
+        'It takes a great deal of courage to stand alone even if you believe in something very strongly.',
       userId: 9,
+      createdAt: '2022-05-11T10:23:30.693Z',
     },
   ]);
 }
