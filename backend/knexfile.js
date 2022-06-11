b7edca9a20a81d8a58888f4b0a76c686e68cb4c2
rkrux <5960750+rkrux@users.noreply.github.com>
2022-06-11 20:15:36 +0530

    config update

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -24,6 +24,7 @@ const knexConfig = {
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
+      charset: 'utf8',
     },
     pool: {
       min: 2,
