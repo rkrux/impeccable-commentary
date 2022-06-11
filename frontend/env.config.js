0d837d4561bde78dbf47b3dec2ea2019117ada3b
rkrux <5960750+rkrux@users.noreply.github.com>
2022-06-11 23:44:58 +0530

    Removed extra slash from URL

--- frontend/env.config.js
+++ frontend/env.config.js
@@ -1,6 +1,6 @@
 export default {
   env: {
-    BASE_API_URL: 'https://impeccable-commentary-backend.herokuapp.com/',
-    WEB_SOCKET_URL: 'wss://impeccable-commentary-backend.herokuapp.com/',
+    BASE_API_URL: 'https://impeccable-commentary-backend.herokuapp.com',
+    WEB_SOCKET_URL: 'wss://impeccable-commentary-backend.herokuapp.com',
   },
 };
